38d9ce793025e8b11828b9bc797dec3fc3147b42
Aleksandra Oftina <aoftina@it-one.ru>
2023-04-18 17:02:05 +0300

    Added Lion class and Test

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class LionTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionTest {
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Spy
+    Feline feline;
+
+    private final String actualSex;
+    private final Boolean expectedIsMane;
+    public LionTest(String actualSex, Boolean expectedIsMane) {
+        this.actualSex = actualSex;
+        this.expectedIsMane = expectedIsMane;
+    }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] dataForSexLion() {
+        return new Object[][] {
+                { "Самец", true},
+                { "Самка", false},
+        };
+    }
+    @Test
+    public void checkUncorrectedLionSex() throws Exception {
+        try{
+            Lion lion = new Lion("Неизвестно");
+
+        } catch (Exception thrown){
+            String expectedResult = "Используйте допустимые значения пола животного - самец или самка";
+            assertEquals(expectedResult, thrown.getMessage());
+        }
+    }
+    @Test
+    public void checkLionSex() throws Exception {
+        Lion lion = new Lion(actualSex);
+        assertEquals(expectedIsMane, lion.doesHaveMane());
+    }
+
+    @Test
+    public void getKittensTest(){
+        Lion lion = new Lion(feline);
+        lion.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens();
+    }
+    @Test
+    public void getFoodTest() throws Exception {
+        Lion lion = new Lion(feline);
+        lion.getFood();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+
 }
