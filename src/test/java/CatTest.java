6952a76ea482eb0009fa0798a6fabf6508126437
Aleksandra Oftina <aoftina@it-one.ru>
2023-04-18 16:58:25 +0300

    Added classes to stage and commit them

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class CatTest {
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Spy
+    Feline predator;
+
+    @Test
+    public void getSoundTestReturnCorrectValue() {
+        Cat cat = new Cat(predator);
+        String actualSound = cat.getSound();
+        assertEquals("Мяу", actualSound);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(predator);
+        cat.getFood();
+        Mockito.verify(predator, Mockito.times(1)).eatMeat();
+    }
+
 }
